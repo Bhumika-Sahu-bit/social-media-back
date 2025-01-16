e3d27dd7920f8af5e2e70b143470bb260c229228
bhumika sahu <bhumi16124@gmail.com>
2025-01-16 23:24:35 +0530

    6-commit

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const Admin = require("../models/Admin");
 
 const router = express.Router();
 
